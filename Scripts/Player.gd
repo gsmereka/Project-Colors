fa24c0932abce78525421781de92f572983892b6
Gabriel Smereka <gabrielsmereka200@gmail.com>
2024-02-18 23:18:50 -0300

    att beam

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -10,18 +10,24 @@ var max_stamina: float = 2000
 #var	stamina_decrease: float = 10
 var	stamina_decrease: float = 0.0
 var stamina_increase: float = 20
+var mana: float = 0
+var max_mana: float = 2000
+var	mana_decrease: float = 20.0
+var mana_increase: float = 20
 var color_array: Array = [Color(1,0,0,1), Color(0,0,1,1), Color(1,1,0,1)]
 var color_selected: int = 1
-var laser
+var laser_sound
 var steps
 var low_steps = preload("res://Sound Effects/andando.mp3")
 var run_steps = preload("res://Sound Effects/correndo.mp3")
-
+var ray
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var left
 var right
 var	angle = 0
+var	can_shot: bool = true
+var shader
 
 func _ready():
 	Global.player_node = self
@@ -29,13 +35,18 @@ func _ready():
 	Global.dimension_list.append(get_parent().get_node("blue_dimension"))
 	Global.dimension_list.append(get_parent().get_node("yellow_dimension"))
 	Global.actual_dimension = get_parent().get_node("yellow_dimension")
-	var shader = get_parent().get_node("Color_Shader")
+	shader = get_parent().get_node("Color_Shader")
 	if shader:
 		shader.material.set_shader_parameter("color", color_array[2])
 	left = get_node("FOV/left")
 	right = get_node("FOV/right")
-	laser = get_node("laser")
+	laser_sound = get_node("laser")
 	steps = get_node("steps")
+	ray = get_node("ray_shader")
+	color_selected = 1
+	Global.actual_dimension = Global.dimension_list[color_selected]
+	if (Global.actual_dimension):
+		Global.actual_dimension.visible = true
 
 func run():
 	if Input.is_action_pressed("Run"):
@@ -55,19 +66,19 @@ func run():
 	return (velocity)
 	
 func change_lantern():
-	var shader = get_parent().get_node("Color_Shader")
 	if (!shader):
 		return
 	if (Input.is_action_just_pressed("Interact")):
-		shader.material.set_shader_parameter("color", color_array[color_selected])
 		if (Global.actual_dimension):
 			Global.actual_dimension.visible = false
-		Global.actual_dimension = Global.dimension_list[color_selected]
-		if (Global.actual_dimension):
-			Global.actual_dimension.visible = true
 		color_selected += 1
 		if (color_selected == len(color_array)):
 			color_selected = 0
+		shader.material.set_shader_parameter("color", color_array[color_selected])
+		Global.actual_dimension = Global.dimension_list[color_selected]
+		if (Global.actual_dimension):
+			Global.actual_dimension.visible = true
+		
 		#Global.color_shader
 	pass
 
@@ -76,24 +87,45 @@ var light_Speed = 0.02
 #0.78539800643921
 #-0.78539800643921
 func atack():
-	if Input.is_action_pressed("Fire"):
-		if (!laser.playing):
-			laser.play()
+	if Input.is_action_pressed("Fire") && can_shot:
+		if (!laser_sound.playing && right.rotation == 0):
+			laser_sound.play()
 		right.rotation -= light_Speed
 		if (right.rotation <= 0):
 			right.rotation = 0
+			ray.visible = true
+			shader.material.set_shader_parameter("color", Color(1,1,1,1))
 		left.rotation += light_Speed
 		if (left.rotation >= 0):
 			left.rotation = 0
 	else:
-		if (laser.playing):
-			laser.stop()
+		shader.material.set_shader_parameter("color", color_array[color_selected])
+		ray.visible = false
+		if (laser_sound.playing):
+			laser_sound.stop()
 		right.rotation += light_Speed
 		if (right.rotation >= 0.78539800643921):
 			right.rotation = 0.78539800643921
 		left.rotation -= light_Speed
 		if (left.rotation <= -0.78539800643921):
 			left.rotation = -0.78539800643921
+
+func control_atack():
+	if left.rotation == 0:
+		if mana > 0:
+			mana -= mana_decrease
+		else:
+			can_shot = false
+	else:
+		if mana < max_mana:
+			mana += mana_increase
+			if (mana > max_mana):
+				mana = max_mana
+	#if (Input.is_action_just_pressed("Fire")):
+		#can_shot = true
+	if (!can_shot && mana == max_mana):
+		can_shot = true
+	pass
 
 func steps_sound():
 	if (Input.is_action_just_pressed("Run")):
@@ -112,6 +144,7 @@ func _physics_process(delta):
 	velocity = run()
 	steps_sound()
 	atack()
+	control_atack()
 	change_lantern()
 	look_at(get_global_mouse_position())
 	move_and_slide()
